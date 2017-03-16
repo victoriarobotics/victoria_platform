d99072f87a268e712e4a5a37c70ca6bfda4e8475
Mark Womack <mwomack@gmail.com>
2017-03-15 23:53:29 -0700

    Organized code, started odom publish code.

--- Arduino/victoria_teensy_ros_node/victoria_teensy_ros_node.ino
+++ Arduino/victoria_teensy_ros_node/victoria_teensy_ros_node.ino
@@ -15,10 +15,13 @@
 
 // Teensy includes
 #define ENCODER_OPTIMIZE_INTERRUPTS
-#include <Encoder.h>  // 
+#include <Encoder.h>
 #include <i2c_t3.h>
 #include <LSM6.h>
 #include <LIS3MDL.h>
+
+// Other includes
+#include <Timer.h>
 
 // ROS includes
 #include <ros.h>
@@ -29,50 +32,44 @@
 #include <std_msgs/String.h>
 
 // Teensy pin definitions
-#define ENCODER_LEFT_PIN_1 29   // Teensy digital pin 29
-#define ENCODER_LEFT_PIN_2 30   // Teensy digital pin 30
+#define BLINK_PIN           13  // Teensy digital pin 13
+#define ENCODER_LEFT_PIN_1  29  // Teensy digital pin 29
+#define ENCODER_LEFT_PIN_2  30  // Teensy digital pin 30
 #define ENCODER_RIGHT_PIN_1 31  // Teensy digital pin 31
 #define ENCODER_RIGHT_PIN_2 32  // Teensy digital pin 32
-#define BUMPER_LEFT_PIN A15      // Teensy analog pin 15
-#define BUMPER_RIGHT_PIN A14     // Teensy analog pin 14
+#define BUMPER_LEFT_PIN     A15 // Teensy analog pin 15
+#define BUMPER_RIGHT_PIN    A14 // Teensy analog pin 14
+
+// Timer
+Timer timer;
 
 // TRex motor controller
 HardwareSerial trex = HardwareSerial();
-
-#define BLINK_DURATION 2000
-#define BLINK_PIN 13 // Teensy digital pin 13 for LED
-int blink_state;
-unsigned long next_blink_time;
-
-char debug_str[80];
+double motor_left_speed;
+double motor_right_speed;
 
 // Victoria constants
+#define TICKS_PER_RADIAN 11 // TODO(mwomack): Need the real value here
 #define TRACK_RADIUS 0.235  // in meters
 #define WHEEL_RADIUS 0.127  // in meters
 #define MAX_SPEED 4.71      // in meters/second
 
 // Bumper sensors
 #define BUMPER_THRESHOLD 300  // Threshold that will indicate robot should stop
-bool bumper_stop = false;
-unsigned int bumper_left_base;
-unsigned int bumper_right_base;
+bool bumper_stop = false; // Set to true if bumpers indicate robot should stop
+unsigned int bumper_left;
+unsigned int bumper_right;
 
 // Motor encoders
 Encoder encoder_left(ENCODER_LEFT_PIN_1, ENCODER_LEFT_PIN_2);
 Encoder encoder_right(ENCODER_RIGHT_PIN_1, ENCODER_RIGHT_PIN_2);
 unsigned long encoder_left_pos;
 unsigned long encoder_right_pos;
-double motor_left_speed;
-double motor_right_speed;
-
-/*
-double ang_v_l[10];
-double ang_v_r[10];
-int velocity_index = 0;
 ros::Time last_encoder_read_time;
-ros::Time next_encoder_read_time;
-ros::Duration next_encoder_read_duration = ros::Duration(3e-3);
-*/
+
+double ang_vel_l[10];
+double ang_vel_r[10];
+int velocity_index = 0;
 
 // IMU and Magnetometer
 LSM6 imu;
@@ -83,68 +80,63 @@ bool mag_err = false;
 // ROS node handle
 ros::NodeHandle ros_nh;
 
-ros::Time last_cmd_vel_time;
-std_msgs::String ros_debug_msg;
-ros::Publisher ros_bumper_pub("bumper_info", &ros_debug_msg);
-ros::Publisher ros_encoder_pub("encoder_info", &ros_debug_msg);
-ros::Publisher ros_imu_pub("imu_info", &ros_debug_msg);
-ros::Publisher ros_magnetometer_pub("magnetometer_info", &ros_debug_msg);
-ros::Publisher ros_debug_pub("v_debug_info", &ros_debug_msg);
-
-void cmdVelCallback(const geometry_msgs::Twist& cmd_vel_msg) {
-  snprintf(debug_str, sizeof(debug_str), "linear: %6f %6f %6f    angular: %6f %6f %6f",
-    cmd_vel_msg.linear.x, cmd_vel_msg.linear.y, cmd_vel_msg.linear.z,
-    cmd_vel_msg.angular.x, cmd_vel_msg.angular.y, cmd_vel_msg.angular.z);
-  ros_debug_msg.data = debug_str;
-  ros_debug_pub.publish(&ros_debug_msg);
-  
-  last_cmd_vel_time = ros_nh.now();
-
-  if (bumper_stop) {
-    return;
-  }
-
-  // linear wheel velocity
-  double vl = cmd_vel_msg.linear.x - (cmd_vel_msg.angular.z * TRACK_RADIUS);
-  double vr = cmd_vel_msg.linear.x + (cmd_vel_msg.angular.z * TRACK_RADIUS);
-
-  // angular velocity
-  double wl = vl / WHEEL_RADIUS;
-  double wr = vr / WHEEL_RADIUS;
-
-  // Translate values to between -1 and 1
-  motor_left_speed = max(-1, min(1, wl / MAX_SPEED));
-  motor_right_speed = max(-1, min(1, wr / MAX_SPEED));
-
-  snprintf(debug_str, sizeof(debug_str), "l: %6f %6f %6f    r: %6f %6f %6f",
-    vl, wl, motor_left_speed,
-    vr, wr, motor_right_speed);
-  ros_debug_msg.data = debug_str;
-  ros_debug_pub.publish(&ros_debug_msg);
-}
-
 // ROS cmd_vel subscriber
 // Threshold, in nanoseconds, to wait for a cmd_vel message else take action
 // to stop robot activity
-#define CMD_VEL_TIMEOUT_THRESHOLD_NS 200000000
+#define CMD_VEL_TIMEOUT_THRESHOLD 0.2 // 200 milliseconds
+ros::Time last_cmd_vel_time;
+bool cmd_vel_timeout_stop = false; // Set to true if time out threshold has been exceeded
 void cmdVelCallback(const geometry_msgs::Twist& twist_msg);
 ros::Subscriber<geometry_msgs::Twist> ros_cmd_vel_sub("cmd_vel", cmdVelCallback);
 
 // ROS Odometry publisher
 nav_msgs::Odometry ros_odom_msg;
 ros::Publisher ros_odom_pub("odom", &ros_odom_msg);
+ros::Time last_odom_publish_time;
 
 // ROS Odometry broadcaster
 geometry_msgs::TransformStamped ros_odom_transform;
 tf::TransformBroadcaster ros_odom_broadcaster;
 
 ros::Time current_time;
-ros::Time last_time;
+ros::Time previous_time;
 
 char ros_odom_header_frame_id[] = "/odom";
 char ros_odom_child_frame_id[] = "/base_link";
 
-//Pose pose;
+// ROS Debug publishers
+#define ENABLE_BUMPER_DEBUG  // Uncomment to publish bumper debug info
+#define ENABLE_ENCODER_DEBUG  // Uncomment to publish encoder debug info
+#define ENABLE_IMU_DEBUG  // Uncomment to publish imu debug info
+#define ENABLE_MAGNETOMETER_DEBUG  // Uncomment to publish magnetometer debug info
+#define ENABLE_TEENSY_DEBUG  // Uncomment to publish teensy debug info
+
+char debug_str[80];
+std_msgs::String ros_debug_msg;
+
+#ifdef ENABLE_BUMPER_DEBUG
+ros::Publisher ros_bumper_debug_pub("bumper_debug", &ros_debug_msg);
+#endif
+
+#ifdef ENABLE_ENCODER_DEBUG
+ros::Publisher ros_encoder_debug_pub("encoder_debug", &ros_debug_msg);
+#endif
+
+#ifdef ENABLE_IMU_DEBUG
+ros::Publisher ros_imu_debug_pub("imu_debug", &ros_debug_msg);
+#endif
+
+#ifdef ENABLE_MAGNETOMETER_DEBUG
+ros::Publisher ros_magnetometer_debug_pub("magnetometer_debug", &ros_debug_msg);
+#endif
+
+#ifdef ENABLE_TEENSY_DEBUG
+ros::Publisher ros_teensy_debug_pub("teensy_debug_info", &ros_debug_msg);
+#endif
+
+// Debug blink
+#define BLINK_DURATION 2000
+int blink_state = LOW;
   
 void setup() {
   // Setup basic I2C master mode pins 18/19, external pullups, 400kHz, 200ms default timeout
@@ -154,14 +146,14 @@ void setup() {
   // Setup IMU and magenetometer that use I2C
   if (imu.init()) {
     imu.enableDefault();
-    // TODO: set timeout?
+    // TODO(mwomack): set timeout?
   } else {
     imu_err = true;
   }
   
   if (mag.init()) {
     mag.enableDefault();
-    // TODO: set timeout?
+    // TODO(mwomack): set timeout?
   } else {
     mag_err = true;
   }
@@ -169,16 +161,31 @@ void setup() {
   // Start serial port to TRex
   trex.begin(19200);
 
-  bumper_left_base = 5000; //analogRead(BUMPER_LEFT_PIN);
-  bumper_right_base = 5000; //analogRead(BUMPER_RIGHT_PIN);
-
-  next_blink_time = millis();
-  blink_state = LOW;
+  // Setup pin configurations
   pinMode(BLINK_PIN, OUTPUT);
 
-  // Initialize ros node
-  ros_nh.initNode();
+  encoder_left_pos = 0.0;
+  encoder_right_pos = 0.0;
+  motor_left_speed = 0.0;
+  motor_right_speed = 0.0;
 
+  // Initialize and connect to ros
+  ros_nh.initNode();
+  while(!ros_nh.connected()) {
+    ros_nh.spinOnce();
+  }
+
+  // Get ros parameters
+  int read_encoders_freq_hz;
+  if (!ros_nh.getParam("read_encoders_freq_hz", &read_encoders_freq_hz)) { 
+    read_encoders_freq_hz = 300;
+  }
+  
+  int publish_odom_freq_hz;
+  if (!ros_nh.getParam("publish_odom_freq_hz", &publish_odom_freq_hz)) { 
+    publish_odom_freq_hz = 100;
+  }
+  
   // Initialize broadcasters
   ros_odom_transform.header.frame_id = ros_odom_header_frame_id;
   ros_odom_transform.child_frame_id = ros_odom_child_frame_id;
@@ -188,151 +195,138 @@ void setup() {
   ros_odom_msg.header.frame_id = ros_odom_header_frame_id;
   ros_odom_msg.child_frame_id = ros_odom_child_frame_id;
   ros_nh.advertise(ros_odom_pub);
-  ros_nh.advertise(ros_bumper_pub);
-  ros_nh.advertise(ros_encoder_pub);
-  ros_nh.advertise(ros_imu_pub);
-  ros_nh.advertise(ros_magnetometer_pub);
-  ros_nh.advertise(ros_debug_pub);
 
   // Initialize ros subscribers
   ros_nh.subscribe(ros_cmd_vel_sub);
+
+  // Setup Timer callbacks
+  timer.every((1.0/read_encoders_freq_hz) * 1000, doReadEncoders);
+  timer.every((1.0/publish_odom_freq_hz) * 1000, doPublishOdom);
+  timer.every(BLINK_DURATION, doBlink);
   
   current_time = ros_nh.now();
-  last_time = current_time;
+  previous_time = current_time;
   last_cmd_vel_time = current_time;
-  //last_encoder_read_time = current_time;
-  //next_encoder_read_time = current_time + next_encoder_read_duration;
-  encoder_left_pos = 0.0;
-  encoder_right_pos = 0.0;
-  motor_left_speed = 0.0;
-  motor_right_speed = 0.0;
+  last_odom_publish_time = current_time;
+  last_encoder_read_time = current_time;
+
+  // Initialize ros debug publishers
+#ifdef ENABLE_BUMPER_DEBUG
+  ros_nh.advertise(ros_bumper_debug_pub);
+  timer.every(500, doBumperDebug);
+#endif
+#ifdef ENABLE_ENCODER_DEBUG
+  ros_nh.advertise(ros_encoder_debug_pub);
+  timer.every(500, doEncoderDebug);
+#endif
+#ifdef ENABLE_IMU_DEBUG
+  ros_nh.advertise(ros_imu_debug_pub);
+  timer.every(500, doImuDebug);
+#endif
+#ifdef ENABLE_MAGNETOMETER_DEBUG
+  ros_nh.advertise(ros_magnetometer_debug_pub);
+  timer.every(500, doMagnetometerDebug);
+#endif
+#ifdef ENABLE_TEENSY_DEBUG
+  ros_nh.advertise(ros_teensy_debug_pub);
+#endif
 }
 
 void loop() {
-  
-    
-  current_time = ros_nh.now();
-
-  // If we have not received any cmd_vel messages for a while,
-  // something is wrong, stop the robot.
-  if (current_time.toNsec() - last_cmd_vel_time.toNsec() 
-        > CMD_VEL_TIMEOUT_THRESHOLD_NS) {
-    // TODO: Do something to stop the robot!
-  }
-  
-  // Read bumper sensors
-  unsigned int new_bumper_left = analogRead(BUMPER_LEFT_PIN);
-  unsigned int new_bumper_right = analogRead(BUMPER_RIGHT_PIN);
-
   // Check bumper thresholds to stop robot
-  if (new_bumper_left < 450 || new_bumper_right < 450) {
-    bumper_stop = true;
+  // TODO(mwomack): Figure out how to support ROS instructing going backwards
+  bumper_left = analogRead(BUMPER_LEFT_PIN);
+  bumper_right = analogRead(BUMPER_RIGHT_PIN);
+  bumper_stop = bumper_left < 450 || bumper_right < 450;
+  if (bumper_stop) {
     motor_left_speed = 0;
     motor_right_speed = 0;
-  } else {
-    bumper_stop = false;
   }
 
-  /*
-  // Read encoders
-  if (current_time > next_encoder_read_time) {
-    unsigned long new_encoder_l_pos = encoder_left.read();
-    unsigned long new_encoder_r_pos = encoder_right.read();
+  // Set current time for all following code
+  current_time = ros_nh.now();
 
-    // TODO handle overflow/rest to 0
-    unsigned long diff_l = new_encoder_l_pos - encoder_left_pos;
-    unsigned long diff_r = new_encoder_r_pos - encoder_right_pos;
-    double diff_t = current_time - last_encoder_read_time;
+  // Update any timer callbacks
+  timer.update();
 
-    ang_vel_l[velocity_index] = (diff_l/TICKS_PER_RADIAN)/diff_t;
-    ang_vel_r[velocity_index] = (diff_r/TICKS_PER_RADIAN)/diff_t;
-    
-    velocity_index = (velocity_index+1)%10;
-    encoder_left_pos = new_encoder_left_pos;
-    encoder_right_pos = new_encoder_right_pos;
-    last_encoder_read_time = current_time;
-    next_encoder_read_time = current_time + next_encoder_read_duration;
+  // Update ROS
+  ros_nh.spinOnce();
+  
+  // If have not received cmd_vel messages in threshold time,
+  // something is wrong, stop the robot.
+  cmd_vel_timeout_stop = (current_time.toSec() - last_cmd_vel_time.toSec()) > CMD_VEL_TIMEOUT_THRESHOLD;
+  if (cmd_vel_timeout_stop) {
+    motor_left_speed = 0;
+    motor_right_speed = 0;
   }
+
+  // Set the motors to the current speed
+  setMotors(motor_left_speed, motor_right_speed);
+
+  // Current time is now the previous time
+  previous_time = current_time;
+}
+
+/*
+ * Callback used by ros_cmd_vel_sub to handle cmd_vel commands.
+ */
+void cmdVelCallback(const geometry_msgs::Twist& cmd_vel_msg) {
+  last_cmd_vel_time = ros_nh.now();
+
+  if (bumper_stop) {
+    // TODO(mwomack): Allow for reverse when bumper_stop
+    return;
+  }
+  
+  // Calculate linear wheel velocity
+  double vl = cmd_vel_msg.linear.x - (cmd_vel_msg.angular.z * TRACK_RADIUS);
+  double vr = cmd_vel_msg.linear.x + (cmd_vel_msg.angular.z * TRACK_RADIUS);
+
+  // Calculate angular velocity
+  double wl = vl / WHEEL_RADIUS;
+  double wr = vr / WHEEL_RADIUS;
+
+  // Calculate motor speed between -1 and 1
+  motor_left_speed = max(-1, min(1, wl / MAX_SPEED));
+  motor_right_speed = max(-1, min(1, wr / MAX_SPEED));
+}
+
+void doReadEncoders() {
+  ros::Time current_time = ros_nh.now();
   unsigned long new_encoder_left_pos = encoder_left.read();
   unsigned long new_encoder_right_pos = encoder_right.read();
 
+  // TODO(mwomack): handle overflow/reset to 0
+  unsigned long diff_l = new_encoder_left_pos - encoder_left_pos;
+  unsigned long diff_r = new_encoder_right_pos - encoder_right_pos;
+  double diff_t = current_time.toSec() - last_encoder_read_time.toSec();
 
-  double dt = (current_time - last_time).toSec();
-  */
-    
-  // Read the bumper sensors
-  snprintf(debug_str, sizeof(debug_str), "L: %d    R: %d",
-    analogRead(BUMPER_LEFT_PIN), analogRead(BUMPER_RIGHT_PIN));
-  ros_debug_msg.data = debug_str;
-  ros_bumper_pub.publish(&ros_debug_msg);
-  
-  // Read the encoders
-  snprintf(debug_str, sizeof(debug_str), "L: %ld    R: %ld",
-    encoder_left.read(), encoder_right.read());
-  ros_debug_msg.data = debug_str;
-  ros_encoder_pub.publish(&ros_debug_msg);
-  
-  // Read the IMU
-  if (imu_err) {
-    ros_debug_msg.data = "IMU failure!";
-  } else {
-    long i2c_start_time = millis();
-    imu.read();
-    long i2c_duration = millis() - i2c_start_time;
-  
-    snprintf(debug_str, sizeof(debug_str), "A: %6d %6d %6d    G: %6d %6d %6d   I2C Duration: %ld",
-      imu.a.x, imu.a.y, imu.a.z,
-      imu.g.x, imu.g.y, imu.g.z,
-      i2c_duration);
-    ros_debug_msg.data = debug_str;
-  }
-  ros_imu_pub.publish(&ros_debug_msg);
+  ang_vel_l[velocity_index] = (diff_l/TICKS_PER_RADIAN)/diff_t;
+  ang_vel_r[velocity_index] = (diff_r/TICKS_PER_RADIAN)/diff_t;
 
-  // Read the Magnetometer
-  if (mag_err) {
-    ros_debug_msg.data = "Magnetometer failure!";
-  } else {
-    long i2c_start_time = millis();
-    mag.read();
-    long i2c_duration = millis() - i2c_start_time;
-  
-    snprintf(debug_str, sizeof(debug_str), "M: %6d %6d %6d   I2C Duration: %ld",
-      mag.m.x, mag.m.y, mag.m.z,
-      i2c_duration);
-    ros_debug_msg.data = debug_str;
-  }
-  ros_magnetometer_pub.publish(&ros_debug_msg);
+  velocity_index = (velocity_index + 1) % 10;
+  encoder_left_pos = new_encoder_left_pos;
+  encoder_right_pos = new_encoder_right_pos;
+  last_encoder_read_time = current_time;
+}
 
+void doPublishOdom() {
+  ros::Time current_time = ros_nh.now();
+  double dt = current_time.toSec() - last_odom_publish_time.toSec();
+  
   // Broadcast odometry transform
   ros_odom_transform.header.stamp = current_time;
 
   // TODO: set all the transform data
 
-  //ros_odom_broadcaster.sendTransform(ros_odom_transform);
+  ros_odom_broadcaster.sendTransform(ros_odom_transform);
   
   // Publish Odometry
   ros_odom_msg.header.stamp = current_time;
   
   // TODO: Set all the odometry data
   
-  //ros_odom_pub.publish(&ros_odom_msg);
-  
-  last_time = current_time;
-  
-  ros_nh.spinOnce();
-
-  setMotors(motor_left_speed, motor_right_speed);
-  
-  // Indicate to outside world that something is going on
-  if (millis() > next_blink_time) {
-    if (blink_state == LOW) {
-      blink_state = HIGH;
-    } else {
-      blink_state = LOW;
-    }
-    digitalWrite(BLINK_PIN, blink_state);
-    next_blink_time = millis() + BLINK_DURATION;
-  }
+  ros_odom_pub.publish(&ros_odom_msg);
 }
 
 /*
@@ -357,15 +351,74 @@ void setMotors(double motor_left_speed, double motor_right_speed) {
   }
   int trex_right_speed = min(127, 127.0 * abs(motor_right_speed));
   
-  snprintf(debug_str, sizeof(debug_str), "cmd_l: %6f %6d %6d    cmd_r: %6f %6d %6d",
-    motor_left_speed, command_byte_left, trex_left_speed,
-    motor_right_speed, command_byte_right, trex_right_speed);
-  ros_debug_msg.data = debug_str;
-  ros_debug_pub.publish(&ros_debug_msg);
-  
   trex.write(command_byte_left);
   trex.write((byte)trex_left_speed);
   trex.write(command_byte_right);
   trex.write((byte)trex_right_speed);
 }
+
+// Callback for blink
+void doBlink() {
+  if (blink_state == LOW) {
+    blink_state = HIGH;
+  } else {
+    blink_state = LOW;
+  }
+  digitalWrite(BLINK_PIN, blink_state);
+}
+
+#ifdef ENABLE_BUMPER_DEBUG
+void doBumperDebug() {
+  // Read the bumper sensors
+  snprintf(debug_str, sizeof(debug_str), 
+    "L: %d    R: %d    STOP: %d", bumper_left, bumper_right, bumper_stop);
+  ros_debug_msg.data = debug_str;
+  ros_bumper_debug_pub.publish(&ros_debug_msg);
+}
+#endif
+
+#ifdef ENABLE_ENCODER_DEBUG
+void doEncoderDebug() {
+  // Read the encoders
+  snprintf(debug_str, sizeof(debug_str), "L: %ld    R: %ld",
+    encoder_left.read(), encoder_right.read());
+  ros_debug_msg.data = debug_str;
+  ros_encoder_debug_pub.publish(&ros_debug_msg);
+}
+#endif
+
+#ifdef ENABLE_IMU_DEBUG
+void doImuDebug() {
+  // Read the IMU
+  if (imu_err) {
+    ros_debug_msg.data = "IMU failure!";
+  } else {
+    imu.read();
+  
+    snprintf(debug_str, sizeof(debug_str), 
+      "A: %6d %6d %6d    G: %6d %6d %6d",
+      imu.a.x, imu.a.y, imu.a.z,
+      imu.g.x, imu.g.y, imu.g.z);
+    ros_debug_msg.data = debug_str;
+  }
+  ros_imu_debug_pub.publish(&ros_debug_msg);
+}
+#endif
+
+#ifdef ENABLE_MAGNETOMETER_DEBUG
+void doMagnetometerDebug() {
+  // Read the Magnetometer
+  if (mag_err) {
+    ros_debug_msg.data = "Magnetometer failure!";
+  } else {
+    mag.read();
+  
+    snprintf(debug_str, sizeof(debug_str),
+      "M: %6d %6d %6d",
+      mag.m.x, mag.m.y, mag.m.z);
+    ros_debug_msg.data = debug_str;
+  }
+  ros_magnetometer_debug_pub.publish(&ros_debug_msg);
+}
+#endif
 
